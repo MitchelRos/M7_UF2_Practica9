49d822e5ebc7a5133b708ec5da622db8d46b5d46
Kevin Sama 22 <113372511+ksama22@users.noreply.github.com>
2022-12-15 20:57:11 +0100

    Kevin a (#10)
    * Exercici 1
    
    * 4

---
+++ exerici1.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+ndarray = np.array([
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+])
+
+
+#Per defecte si no es posa una array
+def CreaArrayComoImagen(arr=ndarray):
+    print(arr)
+    # Retorna la dimesio de la array
+    print("Dimensio Matriu", arr.ndim)
+    # Retorna el tamnany de l'array
+    print("Tamany Matriu", arr.shape)
+    # Retorna el total d'elements de l'array
+    print("Total", arr.size)
+    # Retorna el tipus d'elements d'array
+    print("Tipus", arr.dtype)
+    #Guarda fitxer binary
+    print("Guardar fitxer Binari",np.save("exercici1.npy",arr))
+
